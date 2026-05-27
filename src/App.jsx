eafca6175a32b35bad32ef318e33c3c3e3290b5f
Hinckley Olagunju <kryptonolagunju@gmail.com>
2026-05-27 22:21:01 +0100

    Change return element from div to h1

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,3 @@
 export default function App() {
-  return (
-    <div style={{
-      height: "100vh",
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-      fontSize: "40px"
-    }}>
-      Damilola 😭💕
-    </div>
-  );
+  return <h1>Damilola 😭💕</h1>;
 }
